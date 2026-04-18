0bbd4d4076e4ac4b9841dcacb7ed96b2cc6e193f
pnaessen <pnaessen@student.42lyon.fr>
2026-04-18 15:36:14 +0200

    refactor: enhance RoundRobin structure with active target management and health check loop

--- proxy/balancer.go
+++ proxy/balancer.go
@@ -1,26 +1,64 @@
 package proxy
 
 import (
+	"log"
+	"net"
+	"sync"
 	"sync/atomic"
+	"time"
 )
 
 type RoundRobin struct {
-	targets []string
-	counter uint64
+	allTargets    []string
+	activeTargets []string
+	mu            sync.RWMutex
+	counter       uint64
 }
 
 func NewRoundRobin(targets []string) *RoundRobin {
-	return &RoundRobin{
-		targets: targets,
-		counter: 0,
+	rr := &RoundRobin{
+		allTargets:    targets,
+		activeTargets: targets,
+		counter:       0,
 	}
+
+	go rr.healthCheckLoop()
+
+	return rr
 }
 
 func (rr *RoundRobin) NextTarget() string {
+	rr.mu.RLock()
+	defer rr.mu.RUnlock()
+
+	if len(rr.activeTargets) == 0 {
+		return ""
+	}
 
 	val := atomic.AddUint64(&rr.counter, 1)
+	index := (val - 1) % uint64(len(rr.activeTargets))
 
-	index := (val - 1) % uint64(len(rr.targets))
+	return rr.activeTargets[index]
+}
 
-	return rr.targets[index]
+func (rr *RoundRobin) healthCheckLoop() {
+	for {
+		var healthy []string
+
+		for _, target := range rr.allTargets {
+			conn, err := net.DialTimeout("tcp", target, 2*time.Second)
+			if err != nil {
+				log.Printf("[HealthCheck] Serveur dead : %s", target)
+			} else {
+				conn.Close()
+				healthy = append(healthy, target)
+			}
+		}
+
+		rr.mu.Lock()
+		rr.activeTargets = healthy
+		rr.mu.Unlock()
+
+		time.Sleep(5 * time.Second)
+	}
 }
